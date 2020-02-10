7bea9420e659db1f820cd42013b5de447a285079
Russell grant Johnson <rusljohn@gmail.com>
2020-02-10 15:16:56 +1300

    work started in class today about what we did over the holidays

---
+++ script/script.js
@@ -0,0 +1,34 @@
+console.log('hello world');
+
+// Object storing person's 1 inputted data
+var person1 = {
+	activity1 : '',
+	activity1Hours : 0 ,
+	activity2 : '', 
+	activity2Hours : 0 ,
+	activity3 : '' ,
+	activity3Hours : 0 ,
+	activity4 : '' ,
+	activity4Hours : 0 ,
+	activity5 : '' ,
+	activity5Hours : 0
+};
+// Object storing person's 2 inputted data
+// var person2 = {
+// 	activity1 : '',
+// 	activity1Hours : ,
+// 	activity2 : '', 
+// 	activity2Hours : ,
+// 	activity3 : '' ,
+// 	activity3Hours : ,
+// 	activity4 : '' ,
+// 	activity4Hours : ,
+// 	activity5 : '' ,
+// 	activity5Hours : 
+// };
+
+// Action that the user makes 
+$('#submitUsersInput').click(function(){
+	person1.activity1 = documnent.getElementById('activity1P1');
+	console.log(person1);
+});
